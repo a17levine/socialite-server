58a5639f9cd9759c0e487b4e4e308f014c5e3374
Alex Levine <a17levine@gmail.com>
2014-03-08 17:56:34 -0600

    added code to allow CORS

--- app/controllers/instagrams_controller.rb
+++ app/controllers/instagrams_controller.rb
@@ -1,4 +1,30 @@
 class InstagramsController < ApplicationController
+
+
+	skip_before_filter :verify_authenticity_token
+  before_filter :cors_preflight_check
+  after_filter :cors_set_access_control_headers
+
+  # For all responses in this controller, return the CORS access control headers.
+  def cors_set_access_control_headers
+    headers['Access-Control-Allow-Origin'] = '*'
+    headers['Access-Control-Allow-Methods'] = 'POST, GET, OPTIONS'
+    headers['Access-Control-Max-Age'] = "1728000"
+  end
+
+  # If this is a preflight OPTIONS request, then short-circuit the
+  # request, return only the necessary headers and return an empty
+  # text/plain.
+
+  def cors_preflight_check
+    headers['Access-Control-Allow-Origin'] = '*'
+    headers['Access-Control-Allow-Methods'] = 'POST, GET, OPTIONS'
+    headers['Access-Control-Allow-Headers'] = 'X-Requested-With, X-Prototype-Version'
+    headers['Access-Control-Max-Age'] = '1728000'
+  end
+
+
+
 	def index
 		@instagram_results = RestClient.get "https://api.instagram.com/v1/tags/fullscreen/media/recent?client_id=#{ENV['INSTAGRAM_CLIENT_ID']}"
 		render json: @instagram_results
