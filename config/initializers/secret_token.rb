427246c89a9ce442a24ba41286acf7e19f40f768
Alex Levine <a17levine@gmail.com>
2014-03-08 15:54:25 -0600

    initialized with rails

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+SocialiteServer::Application.config.secret_key_base = '266681f3e85c00b6a6b12287662ab908de46c6b5b1d2a33ed15ba03ba8a63fdfb5d8b9e601e5fde110dc03955c0f1e0aee719411209b7e863cc01393ad2c99f8'
