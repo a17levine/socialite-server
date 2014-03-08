6a66267be58aea589f2c20f0f00bd5edb3888a62
Alex Levine <a17levine@gmail.com>
2014-03-08 17:06:05 -0600

    got twitter to puke out what i needed from twitter index call

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -1,2 +1,14 @@
 class TweetsController < ApplicationController
+	def index
+		#this is gross but will do for now
+		@twitter_client ||= Twitter::REST::Client.new do |config|
+			config.consumer_key        = ENV["TWITTER_CONSUMER_KEY"]
+			config.consumer_secret     = ENV["TWITTER_CONSUMER_SECRET"]
+			config.access_token        = ENV["TWITTER_ACCESS_TOKEN"]
+			config.access_token_secret = ENV["TWITTER_ACCESS_SECRET"]
+		end
+
+		@twitter_results = @twitter_client.search("@fullscreen", :lang => "en", count: 10).to_h.to_json
+		render json: @twitter_results
+	end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,8 @@ SocialiteServer::Application.routes.draw do
   # You can have the root of your site routed with "root"
   # root 'welcome#index'
 
+  get 'twitter/' => "tweets#index"
+
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
 
